55b743f9957c9182865979122d454095642f0a55
jmitchell0528 <jlm0528@yahoo.com>
2017-04-13 21:53:37 -0500

    Completed project, no issues.

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,29 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+
+const initialState = {
+  currentValue: 0
+}
+
+// Constrants (Action Types)
+const INCREMENT = 'counter/INCREMENT'
+
+// Action creators (exported functions)
+export function increment(amount) {
+  return {
+    type: INCREMENT,
+    amount: amount
+  }
+}
+
+// Reducer
+export default function counter( state = initialState, action ) {
+    // return state;
+    switch( action.type ) {
+    case INCREMENT:
+      return { currentValue: state.currentValue + action.amount};
+
+    default:
+      return state
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux' // providing gets store, store calls the reducer and then gives the store to the app
 
+import store from './store'
 import "./index.css";
 
 import App from "./App";
 
 ReactDOM.render(
-	<App />
+	<Provider store={ store }>
+		<App />
+	</Provider>
 	, document.getElementById( 'root' )
 );
